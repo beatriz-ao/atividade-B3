db06f6a493f568a07d5af3b41344f082e6231ee1
beatriz-ao <sbao@discente.ifpe.edu.br>
2026-03-12 10:18:31 -0300

    Update MainActivity.kt

--- ATIVIDADE1/MainActivity.kt
+++ ATIVIDADE1/MainActivity.kt
@@ -3,42 +3,63 @@ package com.example.meuprimeiroprojeto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.meuprimeiroprojeto.ui.theme.MeuPrimeiroProjetoTheme
-
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
-
-import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            Column(modifier = Modifier.padding(16.dp)) {
+
+                MessageCard(
+                    Message("Cacá", "oi", R.drawable._snoopy)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                MessageCard(
+                    Message("Drax", "tudo bem", R.drawable.cachorrinho)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                MessageCard(
+                    Message("Snoopy", "bem vindo", R.drawable.cao2)
+                )
+            }
+        }
+    }
+}
 
 data class Message(
     val author: String,
-    val body: String
+    val body: String,
+    val image: Int
 )
 
 @Composable
 fun MessageCard(msg: Message) {
-
     Row(modifier = Modifier.padding(all = 8.dp)) {
 
         Image(
-            painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
+            painter = painterResource(msg.image),
+            contentDescription = "Foto de perfil",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
@@ -47,11 +68,36 @@ fun MessageCard(msg: Message) {
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
+
             Text(text = msg.author)
 
             Spacer(modifier = Modifier.height(4.dp))
 
             Text(text = msg.body)
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMessageCard() {
+
+    Column(modifier = Modifier.padding(16.dp)) {
+
+        MessageCard(
+            Message("Snoopy", "Oi!", R.drawable._snoopy)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MessageCard(
+            Message("Drax", "Tudo bem?", R.drawable.cachorrinho)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MessageCard(
+            Message("Cacá", "Bem-vindo!", R.drawable.cao2)
+        )
     }
 }
